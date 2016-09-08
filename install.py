5e30b5598bdf966e193674d724b8525552517f66
praveen ms <praveenmukilan@gmail.com>
2016-09-08 13:38:28 +0800

    support for different environments, verify apk built alrealdy

--- install.py
+++ install.py
@@ -6,16 +6,20 @@ import os
 
 HOME=os.getenv("HOME")
 APK_FOLDER=HOME+"/Downloads/Debug"
-BUILD_OUTPUT_FOLDER=HOME+"/HB-Droid-Bee/HBDroidBee/build/outputs/apk"
+BUILD_OUTPUT_FOLDER=HOME+"/HB-Droid-Bee/HBDroidBee/build/outputs/apk/"
+env_d={"staging":"Staging","development":"Development","production":"Production"}
+package_d={"staging":"com.honestbee.bee.staging","development":"com.honestbee.bee.development","production":"com.honestbee.bee"}
+APK=""
 
 class InstallDebug(object):
-
-    
     version=""
-    def __init__(self,version):
+    env=""   
+    def __init__(self,version,env,apk):
         self.version=version
         self.tag="REL_"+version
         self.coreTag=self.tag+"_bee"
+        self.env=env
+        self.apk=apk
 
     def getUdid(self):
         '''
@@ -37,9 +41,10 @@ class InstallDebug(object):
 
     def build(self):
         print "running build"
-        subprocess.check_output("GRADLE_OPTS=-Xmx1024m ./gradlew assembleStagingDebug --stacktrace", shell=True)
+        subprocess.check_output("GRADLE_OPTS=-Xmx1024m ./gradlew assemble"+env_d[self.env]+"Debug --stacktrace", shell=True)
         #if(self.isApkBuilt(self.version,BUILD_OUTPUT_FOLDER)):
-        subprocess.check_output("cp "+BUILD_OUTPUT_FOLDER+"/HBDroidBee-staging-v"+self.version+".apk "+APK_FOLDER, shell=True)
+        #subprocess.check_output("cp "+BUILD_OUTPUT_FOLDER+"/HBDroidBee-staging-v"+self.version+".apk "+APK_FOLDER, shell=True)
+        subprocess.check_output("cp "+BUILD_OUTPUT_FOLDER+self.apk+" "+APK_FOLDER, shell=True)
 
 
     def gitCheckoutBee(self,tag):
@@ -71,7 +76,7 @@ class InstallDebug(object):
         '''
         uninstall any version of the staging apk installed already
         '''
-        command="adb -s "+udid+" uninstall com.honestbee.bee.staging "
+        command="adb -s "+udid+" uninstall "+package_d[self.env]
         print command
         print "uninstalling if there are any installations already"
         output = subprocess.check_output(command, shell=True)
@@ -81,8 +86,8 @@ class InstallDebug(object):
     def installApk(self,udid,version):            
 
         #install apk
-        print "installing apk..."
-        command = "adb -s "+udid+ " install ~/Downloads/Debug/HBDroidBee-staging-v"+version+".apk"
+        print "installing apk..."+self.apk
+        command = "adb -s "+udid+ " install ~/Downloads/Debug/"+self.apk
         print "***** : "+command
         output = subprocess.check_output(command, shell=True)
         #print output
@@ -94,7 +99,7 @@ class InstallDebug(object):
         try:
             os.chdir(path)
             print os.getcwd()
-            subprocess.check_output("ls *staging*"+version+".apk",shell=True)
+            subprocess.check_output("ls "+self.apk,shell=True)
             return True
         except:
             return False
@@ -106,10 +111,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-udid', dest='udid', help='UDID of the device, run `adb devices` to get the udid')
     parser.add_argument('-v', dest='version', help='the version of the apk to install. eg, 3.1.4.7')
+    parser.add_argument('-env', dest='env', help='the environment to build * staging * development * production')
     args = parser.parse_args()
     print args
     version=args.version
-    install=InstallDebug(version)
+    #env if given, else defaults to Staging
+    env=args.env if bool(args.env) else "staging"
+    APK="HBDroidBee-"
+    APK=APK+env+"-v"+version+".apk"
+    print "expected apk :"+APK
+    install=InstallDebug(version,env,APK)
     isBuilt = install.isApkBuilt(version,APK_FOLDER)
     print "isBuilt? "
     print isBuilt
